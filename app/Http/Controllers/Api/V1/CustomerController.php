9207f866a644216866088fc21a06984f75019d9b
Darrylwin <logossoudarryl20@gmail.com>
2025-06-07 23:03:16 +0000

    refactor CustomerController to enhance code clarity and maintainability

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -8,15 +8,21 @@ use App\Http\Requests\UpdateCustomerRequest;
 use App\Http\Resources\V1\CustomerResource;
 use App\Http\Resources\V1\CustomerCollection;
 use App\Models\Customer;
+use Illuminate\Http\Request;
+use App\Services\V1\CustomerQuery;
 
 class CustomerController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        return new CustomerCollection(Customer::paginate());
+        $filter = new CustomerQuery();
+        $queryItems = $filter->transform($request); // ['column', 'operator, 'value']
+
+        if (count($queryItems) === 0) return new CustomerCollection(Customer::paginate());
+        else return new CustomerCollection(Customer::where($queryItems)->paginate());
     }
 
     /**
